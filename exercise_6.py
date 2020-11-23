565dbd2573ad2264d12207000dfb955fdfca5cb7
Piotr Michna <pm@piotrmichna.pl>
2020-11-23 21:25:10 +0100

    definition aplication fun movies()

--- exercise_6.py
+++ exercise_6.py
@@ -9,5 +9,23 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/movies', methods=['GET', 'POST'])
+def movies():
+    if request.method == 'GET':
+        return render_template('movies.html', title='', result='')
+    else:
+        if request.form["title"] == '':
+            return render_template('movies.html', title=request.form["title"], result='Empty title!')
+        else:
+            if request.form["title"] in exam.movies['favourite']:
+                result = 'favourite'
+            elif request.form["title"] in exam.movies['hated']:
+                result = 'hated'
+            else:
+                result = 'no such movie!'
+
+        return render_template('movies.html', title=request.form["title"], result=result)
+
+
 app.run(debug=True)
 
