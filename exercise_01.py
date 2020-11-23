dde959ae6ed351ce3279f4ce82d1a76ed8349fab
Piotr Michna <pm@piotrmichna.pl>
2020-11-23 08:38:56 +0100

    test fun. and formating code

--- exercise_01.py
+++ exercise_01.py
@@ -1,9 +1,19 @@
 def shorten(txt):
-    sh_str=str(txt)
-    sh_str=sh_str.upper()
-    sh_str=""
+    sh_str = str(txt)
+    words = sh_str.split(' ')
+    sh_str = ""
     for word in words:
-        sh_str+=word[0]
+        sh_str += word[0]
 
-    sh_str = sh_str.split(' ')
+    sh_str = sh_str.upper()
     return sh_str
+
+
+shortened = shorten("Don't repeat yourself")
+print(shortened)
+
+shortened = shorten("Read the fine manual")
+print(shortened)
+
+shortened = shorten("All terrain armoured transport")
+print(shortened)
